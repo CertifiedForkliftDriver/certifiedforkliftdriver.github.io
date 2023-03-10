095809a50722b22b4af5f0a7da5fb7e09b117015
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2023-03-10 13:55:19 -0800

    added database
    added database storage games

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-import { collection, addDoc } from "firebase/firestore"; 
 
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+    import { collection, addDoc } from "firebase/firestore"; 
 
-// Add a second document with a generated ID.
-import { addDoc, collection } from "firebase/firestore"; 
+    try {
+    const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
 
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Alan",
-    middle: "Mathison",
-    last: "Turing",
-    born: 1912
-  });
+    // Add a second document with a generated ID.
+    import { addDoc, collection } from "firebase/firestore"; 
 
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+    try {
+    const docRef = await addDoc(collection(db, "users"), {
+        first: "Alan",
+        middle: "Mathison",
+        last: "Turing",
+        born: 1912
+    });
 
-import { collection, getDocs } from "firebase/firestore"; 
+    console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
 
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
+    import { collection, getDocs } from "firebase/firestore"; 
+
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    });
+
