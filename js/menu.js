692817cfb46ac87afd7af3477caf4615321c7562
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2023-03-24 21:09:37 -0700

    trying

--- js/menu.js
+++ js/menu.js
@@ -20,14 +20,10 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase
 
 const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app)
+
 
 // Handle sign-up form submit
-const loginForm = document.getElementById('login-form');
-const loginBtn = document.getElementById('login-btn');
-const signupBtn = document.getElementById('signup-btn');
 
-// Initialize Firebase
 const firebaseConfig = {
           apiKey: "AIzaSyBP_8A81Fbfmake_3Qg_5KhWd6-1XtfM0k",
           authDomain: "gamebox-381701.firebaseapp.com",
@@ -36,73 +32,55 @@ const firebaseConfig = {
           messagingSenderId: "745560099853",
           appId: "1:745560099853:web:5b2699432e6cfec03bd944",
           measurementId: "G-GC71CVBP6X"
-  // Your Firebase config object here
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const authenticate = firebase.auth();
 
-// Function to handle login and sign up form submission
+const auth = firebase.auth();
 
+const loginForm = document.querySelector("#login-form");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
 
-// Function to sign up a new user
-function signUpUser(email, password) {
-  auth.createUserWithEmailAndPassword(email, password)
+const signupForm = document.querySelector("#signup-form");
+const signupEmailInput = document.querySelector("#signup-email-input");
+const signupPasswordInput = document.querySelector("#signup-password-input");
+
+const loginButton = document.querySelector("#login-button");
+const signupButton = document.querySelector("#signup-button");
+const logoutButton = document.querySelector("#logout-button");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
     .then(() => {
-      console.log("User created successfully!");
-      // Redirect the user to the home page or dashboard
-      // Here you can add your own logic to redirect to the appropriate page
+      console.log("User logged in");
     })
     .catch((error) => {
-      console.error("Error creating user: ", error);
-      // Show an error message to the user
-      // Here you can add your own logic to display the error message to the user
+      console.error(error);
     });
-}
+});
 
-// Function to handle the sign-up form submission
-function handleSignUpSubmit(event) {
-  event.preventDefault();
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signupEmailInput.value;
+  const password = signupPasswordInput.value;
 
-  // Get the user's email and password from the form
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-
-  // Call the signUpUser function with the email and password
-  signUpUser(email, password);
-}
-
-// Function to log in an existing user
-function logInUser(email, password) {
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-      console.log("User logged in successfully!");
-      // Redirect the user to the home page or dashboard
-      // Here you can add your own logic to redirect to the appropriate page
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("User created: ", user.uid);
     })
     .catch((error) => {
-      console.error("Error logging in user: ", error);
-      // Show an error message to the user
-      // Here you can add your own logic to display the error message to the user
+      console.error(error);
     });
-}
+});
 
-// Function to handle the login form submission
-function handleLoginSubmit(event) {
-  event.preventDefault();
 
-  // Get the user's email and password from the form
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-
-  // Call the logInUser function with the email and password
-  logInUser(email, password);
-}
-
-// Add an event listener to the sign-up button
-const signUpButton = document.querySelector("#signup");
-signUpButton.addEventListener("click", handleSignUpSubmit);
-
-// Add an event listener to the login button
-const loginButton = document.querySelector("#login");
-loginButton.addEventListener("click", handleLoginSubmit);
