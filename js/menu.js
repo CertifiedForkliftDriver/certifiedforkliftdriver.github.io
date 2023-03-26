a4e6914c2b2309559abc1d66759e974f8afbe79f
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2023-03-26 15:06:33 -0700

    added xue login page, and added neiel's login functionalites to it merging it with main
    merged code

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,26 @@
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('.dropdown_menu');
+// This is the feature that allows responsive menu changes in the nav bar when you shrink the window size
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
 toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle('open')
-  const isOpen = dropDownMenu.classList.contains('open')
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
 
-  toggleBtnIcon.classList = isOpen 
-  ? 'fa-solid fa-xmark'
-  : 'fa-solid fa-bars'
+    toggleBtnIcon.classList = isOpen 
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
 }
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+})
+//Getting started button linked with login .html file
+
