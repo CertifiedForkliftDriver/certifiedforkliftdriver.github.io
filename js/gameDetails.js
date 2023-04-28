3af4a72d3b9162e1fc759c609a907d04c4ead7aa
jgromo <54650393+jgromo@users.noreply.github.com>
2023-04-27 22:40:50 -0700

    Add files via upload

---
+++ js/gameDetails.js
@@ -0,0 +1,80 @@
+// We need this file in order to display games as a separate html file (same window though)
+
+const gameDetailsContainer = document.getElementById('game-details-container');
+
+async function loadGameDetails(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const gameId = urlParams.get('id');
+    const result = await fetch(`https://api.rawg.io/api/games/${gameId}?key=6070e88e166c424cb153bfa25affa500`);
+    const gameDetails = await result.json();
+    const screenshotLink = await fetch(`https://api.rawg.io/api/games/${gameId}/screenshots?key=6070e88e166c424cb153bfa25affa500`);
+    const gameScreenshots = await screenshotLink.json();
+    displayGameDetails(gameDetails, gameScreenshots);
+}
+
+function displayGameDetails(details, screenshots){
+    // gameDetailsContainer.innerHTML = `
+    // <div class = "game-poster">
+    //     <img src = "${(details.background_image != null) ? details.background_image : "images/image_not_found.png"}" alt = "game poster">
+    // </div>
+    // <div class = "game-info">
+    //     <h3 class = "game-title">${details.name}</h3>
+    //     <p class = plot><b>Description:</b>${details.description}</p>
+    //     <ul class = "game-misc-info">
+    //         <li class = "rated">Ratings: ${details.rating}</li>
+    //         <li class = "released">Released: ${details.released}</li>
+    //     </ul>
+    //     <p class = "genre"><b>Genres:</b> ${details.genres[0].name}</p>
+    // </div>
+    // `;
+    gameDetailsContainer.innerHTML = `
+    <div class="banner mb-3">
+        <div class="box">
+            <img src="${(details.background_image != null) ? details.background_image : "images/image_not_found.png"}" alt = "game poster">
+            <div class="text">
+            <h3 class= "game-title">${details.name}</h3>
+            <p>Rating: ${details.rating}</p>
+            <p>
+               Released: ${details.released}
+            </p>
+            </div>
+        </div>
+    </div>
+    <div class="log mb-3">
+        <div class="pic mb-3">
+            <img src="${screenshots.results[0].image}" >
+            <img src="${screenshots.results[1].image}" >
+        </div>
+        <div class="tit mb-3">Description</div>
+        <p class="mb-3">
+            ${details.description}
+        </p>
+    </div>
+    <div class="user mb-3">
+        <div class="item">
+            <h1>Developer</h1>
+            <img src="${details.developers[0].image_background}">
+            <h3>${details.developers[0].name}</h3>
+        </div>
+    </div>
+    `
+}
+
+loadGameDetails();
+
+/* <div class="user mb-3">
+        <div class="item">
+            <h1>Developer</h1>
+            <img src="${details.developers[0].image_background}">
+            <h3>${details.developers[0].name}</h3>
+        </div>
+    </div> */
+
+/*<div class="game-info">
+                <h3 class = "game-title">${details.name}</h3>
+                <ul class = "game-misc-info">
+                    <li class = "rated">Rating: ${details.rating}</li>
+                    <li class = "released">Released: ${details.released}</li>
+                </ul>
+                <p>${details.genres[0].name} | ${details.genres[1].name}</p>
+            </div>*/
