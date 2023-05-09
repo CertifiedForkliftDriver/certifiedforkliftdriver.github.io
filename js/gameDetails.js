f57cf0e76457ee4b5e15f2723dba69844c6b3c4f
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2023-05-08 17:43:13 -0700

    adding joels game info page

--- js/gameDetails.js
+++ js/gameDetails.js
@@ -1,9 +1,7 @@
-// We need this file in order to display games as a separate html file (same window though)
-
-
-
 const gameDetailsContainer = document.getElementById('gameContainer');
+const apiKey = '165cef52ff124c82ae9fe6b59d64a946';
 
+const apiEndpoint = 'https://api.rawg.io/api/games?key=165cef52ff124c82ae9fe6b59d64a946';
 async function loadGameDetails(){
     const urlParams = new URLSearchParams(window.location.search);
     const gameId = urlParams.get('id');
@@ -15,84 +13,122 @@ async function loadGameDetails(){
     
 }
 
+async function getGameDevelopers(gameId) {
+    const response = await fetch(`https://api.rawg.io/api/games/${gameId}/development-team?key=165cef52ff124c82ae9fe6b59d64a946`);
+    const data = await response.json();
+    return data.results.map(result => ({
+      name: result.name,
+      image: result.image,
+    }));
+  }
+
+  function getScreenshotUrl(screenshots, index, fallback = "images/image_not_found.png") {
+    if (screenshots.results[index]) {
+      return screenshots.results[index].image;
+    }
+    return fallback;
+  }
 
 
-function displayGameDetails(details, screenshots){
-    // gameDetailsContainer.innerHTML = `
-    // <div class = "game-poster">
-    //     <img src = "${(details.background_image != null) ? details.background_image : "images/image_not_found.png"}" alt = "game poster">
-    // </div>
-    // <div class = "game-info">
-    //     <h3 class = "game-title">${details.name}</h3>
-    //     <p class = plot><b>Description:</b>${details.description}</p>
-    //     <ul class = "game-misc-info">
-    //         <li class = "rated">Ratings: ${details.rating}</li>
-    //         <li class = "released">Released: ${details.released}</li>
-    //     </ul>
-    //     <p class = "genre"><b>Genres:</b> ${details.genres[0].name}</p>
-    // </div>
-    // `;
+    function getRatingClass(score, isMetacritic = false) {
+        if (isMetacritic) {
+          if (score >= 85) {
+            return "rating-green";
+          }else if (score < 85 && score >= 75) {
+            return "rating-lightgreen";
+          } else if (score < 75 && score >= 60) {
+            return "rating-orange";
+          } else {
+            return "rating-red";
+          }
+        } else {
+          if (score >= 4.5) {
+            return "rating-green";
+          } else if (score >= 4.0) {
+            return "rating-lightgreen";
+          } else if (score >= 3) {
+            return "rating-orange";
+          } else {
+            return "rating-red";
+          }
+        }
+      }
+  
+async function displayGameDetails(details, screenshots){
+  const developers = await getGameDevelopers(details.id);  
     gameDetailsContainer.innerHTML = `
-    <div class="banner mb-3" >
-        <div class="game-image-container">
-        <img src="${screenshots.results[3].image}">
+    <div class="banner mb-3">
+    <div class="game-image-container">
+    <img src="${(details.background_image != null) ? details.background_image : "images/image_not_found.png"}" alt = "game poster">
         </div>
         <div class="box">
-            
-            <img src="${(details.background_image != null) ? details.background_image : "images/image_not_found.png"}" alt = "game poster">
+        
             <div class="text">
-            <h3 class= "game-title">${details.name}</h3>
-            <p>Rating: ${details.rating}</p>
+            <h3 class= "game_title">${details.name}</h3>
+            <p class="rating-text">Rating: <span class="${getRatingClass(details.rating)}">${details.rating}</span></p>
+            <p class="rating-text">Metacritic: <span class="${getRatingClass(details.metacritic, true)}">${details.metacritic}</span></p>
+            <p>Genre: ${
+                (details.genres && details.genres.length > 0)
+                  ? details.genres.map(genre => genre.name).join(" / ")
+                  : "N/A"
+              }</p>
             <p>
-               Released: ${details.released}
+               Released: ${(details.released !=null) ? details.released: "N/A"}
             </p>
-            <button id="fav-btn" class="action_btn" href="html/favorites.html">Favorite Game</button>
+            <p>Platforms: ${details.platforms.map(platform => platform.platform.name).join(', ')}</p>
+            <button class="btn1" href="html/favorites.html">Favorite Game</button>
             </div>
         </div>
     </div>
     <div class="log mb-3">
-        <div class="pic mb-3">
-            <img src="${screenshots.results[0].image}" >
-            <img src="${screenshots.results[1].image}" >
-        </div>
+    <div class="pic mb-3">
+    <img src="${getScreenshotUrl(screenshots, 0)}">
+    <img src="${getScreenshotUrl(screenshots, 1)}">
+    <img src="${getScreenshotUrl(screenshots, 2)}">
+    <img src="${getScreenshotUrl(screenshots, 3)}">
+    <img src="${getScreenshotUrl(screenshots, 4)}">
+    </div>
+
+               
         <div class="tit mb-3">Description</div>
         <p class="mb-3">
             ${details.description}
         </p>
-    </div>
-    <div class="user mb-3">
-        <div class="item">
-            <h1>Developer</h1>
-            <img src="${details.developers[0].image_background}">
-            <h3>${details.developers[0].name}</h3>
-        </div>
-    </div>
+    </div>`
+    if (developers.length > 0) {
+        let developersHtml = `<div class="user mb-3">`;
+    
+        // Get up to the first 4 developers and loop through them
+        developers.slice(0, 4).forEach(developer => {
+          developersHtml += `
+          
+            
+              <div class="item">
+                <img src="${developer.image}">
+                <h3>${developer.name}</h3>
+                <span>Game engineer</span>
+                <div class="developers-info">
+            <p>These are the developers who have worked on this game.</p>
+            </div>  
+            </div>
+           
+           
+          `;
+        });
+    
+        developersHtml += '</div>';
+        gameDetailsContainer.innerHTML += developersHtml;
+      } else {
+        // Display a "No developer info" message when there are no developers
+        gameDetailsContainer.innerHTML += `
+          <div class="no-dev-info">
+            <p>No developer info</p>
+          </div>
+        `;
+      }
     
     
-    `
 }
 
 
 loadGameDetails();
-
-
-
-/* <div class="user mb-3">
-        <div class="item">
-            <h1>Developer</h1>
-            <img src="${details.developers[0].image_background}">
-            <h3>${details.developers[0].name}</h3>
-        </div>
-    </div> */
-
-/*<div class="game-info">
-                <h3 class = "game-title">${details.name}</h3>
-                <ul class = "game-misc-info">
-                    <li class = "rated">Rating: ${details.rating}</li>
-                    <li class = "released">Released: ${details.released}</li>
-                </ul>
-                <p>${details.genres[0].name} | ${details.genres[1].name}</p>
-            </div>*/
-
-
-//favorite function 
